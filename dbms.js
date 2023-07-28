78cd29d9fefe41eec5df576b6e19b91d2e025b81
Ryosei Okayama <ryoseistar@gmail.com>
2023-07-28 19:31:56 -0400

    Finished with Filter

--- dbms.js
+++ dbms.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mysql = require('mysql2');
 var bodyParser = require("body-parser");
+var querystring = require("querystring");
 var app = express();
 
 app.set("view engine", "ejs");
@@ -14,11 +15,12 @@ var pool = mysql.createPool({
     database : 'tree_db'
 });
 
-app.get("/search", function(req, res){
+app.get("/search_trees", function(req, res){
 
     let rainfall_min = req.query.rainfall_min;
+    let rainfall_max = req.query.rainfall_max
     let altitude_min = req.query.altitude_min;
-    let aaa = req.query.aaa;
+    let altitude_max = req.query.altitude_max;
     var q = `SELECT botanical_name, 
                 MIN(rainfall_min) AS minimum_rainfall,
                 MAX(rainfall_max) AS maximum_rainfall,
@@ -29,14 +31,30 @@ app.get("/search", function(req, res){
 	            ON climatic_zone.tree_id = tree.id
             JOIN climatic
 	            ON climatic.id = climatic_zone.climatic_id
-            WHERE rainfall_max > "${rainfall_min}" 
-                AND altitude_max > "${altitude_min}"
+            WHERE rainfall_min <= "${rainfall_min}" 
+                AND rainfall_max >= "${rainfall_max}"
+                AND altitude_min <= "${altitude_min}"
+                AND altitude_max >= "${altitude_max}"
             GROUP BY tree.id
             ORDER BY tree.id`
-    console.log(q)
-    pool.query(q, function(req, res){
-        console.log(res)
-    })
+    
+    pool.query(q, function(err, results){
+        console.log(results)
+        res.render('search_result', {title: 'Tree List', treeData: results});
+        
+    });
+})
+
+app.post("/search_trees", function(req, res){
+    var search_info = {
+        rainfall_min: req.body.rainfall_min,
+        rainfall_max: req.body.rainfall_max,
+        altitude_min: req.body.altitude_min,
+        altitude_max: req.body.altitude_max
+    }
+    const qs = querystring.stringify(search_info);
+    res.redirect("/search_trees?" + qs);
+
 })
 
 app.post("/insert_tree", function(req, res){
@@ -56,7 +74,7 @@ app.post("/update_tree", function(req, res){
     var tree_to_update = req.body.botanical;
     var q = `SELECT botanical_name FROM tree WHERE botanical_name= "${tree_to_update}"`;
 
-    pool.query(q, function(err, results){
+    pool.query(q, function(err, result){
         if (err) throw err;
         var tree_name = results[0].botanical_name
         res.render('dbms_update_tree', {tree_name: tree_name});
